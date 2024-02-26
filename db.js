0e38fd3d04798d3951bc1c0f5d14244842faa7d7
qwertythome <112771120+qwertythome@users.noreply.github.com>
2024-02-26 10:34:04 +0200

    error again

---
+++ db.js
@@ -0,0 +1,63 @@
+let product = [
+    {
+        title: "Banana",
+        price: 2,
+        id: 0
+    },
+    {
+        title: "Orange",
+        price: 3,
+        id: 1
+    },
+    {
+        title: "Chocolate Bar",
+        price: 1.5,
+        id: 2
+    },
+    {
+        title: "Coffee Beans",
+        price: 8,
+        id: 3
+    },
+    {
+        title: "Milk Chocolate",
+        price: 4,
+        id: 4
+    },
+    {
+        title: "Cheese",
+        price: 6,
+        id: 5
+    },
+    {
+        title: "Tomato",
+        price: 1,
+        id: 6
+    },
+    {
+        title: "Spinach",
+        price: 2.5,
+        id: 7
+    },
+    {
+        title: "Salmon",
+        price: 12,
+        id: 8
+    },
+    {
+        title: "Peanut Butter",
+        price: 5,
+        id: 9
+    },
+    {
+        title: "Bread",
+        price: 2,
+        id: 10
+    },
+    {
+        title: "Yogurt",
+        price: 3.5,
+        id: 11
+    }
+]
+export default product;
--- index.js
+++ index.js
@@ -1,46 +1,22 @@
-const bodylist = document.querySelector('.grid');
-const xhr = new XMLHttpRequest();
+const bodylist=document.querySelector(".grid")
+fetch("http://localhost:3000/api/allproducts")
+.then(data=>data.json()
+.then(res=>bild(res.products)))
+function bild(res){
+    let html = "";
+    res.forEach((i)=>{ 
 
-xhr.open('GET', 'http://localhost:3000/api/allproducts', true);
-
-xhr.onload = function () {
-    if (xhr.status >= 200 && xhr.status < 300) {
-        try {
-            const res = JSON.parse(xhr.responseText);
-            if (res && res.products && Array.isArray(res.products)) {
-                bild(res.products);
-            } else {
-                console.error("Некорректные данные от сервера:", res);
-            }
-        } catch (error) {
-            console.error("Ошибка парсинга JSON:", error);
-        }
-    } else {
-        console.error("Ошибка запроса:", xhr.statusText);
-    }
-};
-
-xhr.onerror = function () {
-    console.error("Ошибка сети");
-};
-
-xhr.send();
-
-function bild(res) {
-    console.log(res.length);
-    var html = "";
-    for (var i = 0; i < res.length; i++) {
-        html += `
-            <div class="card">
+      html +=`
+   <div class="card">
                 <div class="text">
-                    name:${res[i].title}
-                    price:${res[i].price}
+                    name:${i.title}
+                    price:${i.price}
                 </div>
                 <div class="fal">
                     <span class="activ material-symbols-outlined activ">favorite</span> 
                     <span class="activdis material-symbols-outlined">thumb_down</span>          
                 </div>
-            </div>`;
-    }
+            </div>`
+    })
     bodylist.innerHTML = html;
 }
