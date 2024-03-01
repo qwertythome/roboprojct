08de1f235ed41b79161d4880fe21a8b53ebbc3d0
qwertythome <112771120+qwertythome@users.noreply.github.com>
2024-03-01 23:36:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-const bodylist=document.querySelector(".grid")
-fetch("http://localhost:3000/api/allproducts")
-.then(data=>data.json()
-.then(res=>bild(res.products)))
-function bild(res){
-    let html = "";
-    res.forEach((i)=>{ 
+const bodylist = document.querySelector(".grid");
 
-      html +=`
-   <div class="card">
-                <div class="text">
-                    name:${i.title}
-                    price:${i.price}
-                </div>
-                <div class="fal">
-                    <span class="activ material-symbols-outlined activ">favorite</span> 
-                    <span class="activdis material-symbols-outlined">thumb_down</span>          
-                </div>
-            </div>`
-    })
-    bodylist.innerHTML = html;
+fetch("http://localhost:3000/api/allproducts")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((res) => {
+    console.log("Відповідь:", res);
+    build(res.products);
+  });
+
+function build(products) {
+  let html = "";
+  for (let i = 0; i < products.length; i++) {
+    html += `
+      <div class="card">
+        <div class="text">
+          name:${products[i].title}
+          price:${products[i].price}
+        </div>
+        <div class="fal">
+          <span class="activ material-symbols-outlined activ">favorite</span> 
+          <span class="activdis material-symbols-outlined">thumb_down</span>          
+        </div>
+      </div>`;
+  }
+  bodylist.innerHTML = html;
 }
